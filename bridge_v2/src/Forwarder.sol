eaaa47dd53251aaa57bc5b1cd97e064cc79eb7c9
yohanelly95 <yohan.nelson@gmail.com>
2024-03-22 12:00:31 +0530

    chore: add razor bridge-v2 contracts

---
+++ bridge_v2/src/Forwarder.sol
@@ -0,0 +1,112 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/utils/Address.sol";
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "./interface/IForwarder.sol";
+
+contract Forwarder is AccessControlEnumerable, Pausable, IForwarder {
+    using Address for address;
+
+    bytes32 public constant FORWARDER_ADMIN_ROLE = keccak256("FORWARDER_ADMIN_ROLE");
+    bytes32 public constant TRANSPARENT_FORWARDER_ROLE = keccak256("TRANSPARENT_FORWARDER_ROLE");
+    bytes32 public constant PAUSE_ROLE = keccak256("PAUSE_ROLE");
+
+    address public resultManager;
+    bytes4 public resultGetterSelector;
+    bytes4 public updateSelector;
+    bytes4 public validateSelector;
+
+    event PermissionSet(address sender);
+    event PermissionRemoved(address sender);
+
+    error NoSelectorPresent();
+    error ZeroAddress();
+
+    modifier checkSelector(bytes4 selector) {
+        if (selector == bytes4(0)) revert NoSelectorPresent();
+        _;
+    }
+
+    constructor(address _resultManager) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        if (_resultManager == address(0)) revert ZeroAddress();
+        resultManager = _resultManager;
+    }
+
+    /// @notice Set result manager contract address
+    /// @dev Allows admin to update result manager
+    /// @param _resultManager new result manager address
+    function setResultManager(address _resultManager) external onlyRole(FORWARDER_ADMIN_ROLE) {
+        if (_resultManager == address(0)) revert ZeroAddress();
+        resultManager = _resultManager;
+    }
+
+    /// @notice Set resultGetter Selector
+    /// @dev Allows admin to set resultGetter Selector
+    /// @param _resultGetterSelector resultGetter Selector
+    function setResultGetterSelector(bytes4 _resultGetterSelector) external onlyRole(FORWARDER_ADMIN_ROLE) {
+        resultGetterSelector = _resultGetterSelector;
+    }
+
+    /// @notice Set update selector
+    /// @dev Allows admin to set update selector
+    /// @param _updateSelector update selector
+    function setUpdateSelector(bytes4 _updateSelector) external onlyRole(FORWARDER_ADMIN_ROLE) {
+        updateSelector = _updateSelector;
+    }
+
+    /// @notice Set validate selector
+    /// @dev Allows admin to set validate selector
+    /// @param _validateSelector validate selector
+    function setValidateSelector(bytes4 _validateSelector) external onlyRole(FORWARDER_ADMIN_ROLE) {
+        validateSelector = _validateSelector;
+    }
+
+    /// @notice pause the contract
+    function pause() external onlyRole(PAUSE_ROLE) {
+        Pausable._pause();
+    }
+
+    /// @notice unpause the contract
+    function unpause() external onlyRole(PAUSE_ROLE) {
+        Pausable._unpause();
+    }
+
+    /**
+     * @notice Updates the result based on the provided data and returns the latest result
+     * @param data bytes data required to update the result
+     * @return result of the collection, its power and timestamp
+     */
+    function updateAndGetResult(
+        bytes calldata data
+    ) external whenNotPaused checkSelector(updateSelector) onlyRole(TRANSPARENT_FORWARDER_ROLE) returns (uint256, int8, uint256) {
+        bytes memory returnData = resultManager.functionCall(abi.encodePacked(updateSelector, data));
+        return abi.decode(returnData, (uint256, int8, uint256));
+    }
+
+    /**
+     * @dev using the hash of collection name, clients can query the result of that collection
+     * @param name bytes32 hash of the collection name
+     * @return result of the collection and its power
+     */
+    function getResult(
+        bytes32 name
+    ) external view whenNotPaused checkSelector(resultGetterSelector) onlyRole(TRANSPARENT_FORWARDER_ROLE) returns (uint256, int8, uint256) {
+        bytes memory returnData = resultManager.functionStaticCall(abi.encodePacked(resultGetterSelector, name));
+        return abi.decode(returnData, (uint256, int8, uint256));
+    }
+
+    /**
+     * @dev validates the result based on the provided data and returns the validity
+     * @param data bytes data required to validate the result
+     * @return validity of the result
+     */
+    function validateResult(
+        bytes calldata data
+    ) external view whenNotPaused checkSelector(validateSelector) onlyRole(TRANSPARENT_FORWARDER_ROLE) returns (bool, uint256, int8, uint256) {
+        bytes memory returnData = resultManager.functionStaticCall(abi.encodePacked(validateSelector, data));
+        return abi.decode(returnData, (bool, uint256, int8, uint256));
+    }
+}
---
+++ bridge_v2/src/ResultManager.sol
@@ -0,0 +1,187 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/access/AccessControlEnumerable.sol";
+import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "@openzeppelin/contracts/utils/cryptography/MerkleProof.sol";
+
+contract ResultManager is AccessControlEnumerable {
+    struct Value {
+        int8 power;
+        uint16 collectionId;
+        bytes32 name;
+        uint256 value;
+        uint256 lastUpdatedTimestamp;
+    }
+
+    bytes32 public constant RESULT_MANAGER_ADMIN_ROLE =
+        keccak256("RESULT_MANAGER_ADMIN_ROLE");
+    bytes32 public constant FORWARDER_ROLE = keccak256("FORWARDER_ROLE");
+
+    address public signerAddress;
+
+    /// @notice mapping for Collection name -> Value Info
+    mapping(bytes32 => Value) private _collectionResults;
+
+    event SignerUpdated(
+        address sender,
+        address indexed prevSigner,
+        address indexed newSigner
+    );
+
+    event ResultUpdated(Value value);
+
+    error InvalidSignature();
+    error InvalidMerkleProof();
+    error ZeroResult();
+    error ZeroAddress();
+
+    constructor(address _signerAddress) {
+        _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        if (_signerAddress == address(0)) revert ZeroAddress();
+        signerAddress = _signerAddress;
+    }
+
+    /**
+     * @notice Updates the signer address
+     */
+    function updateSignerAddress(
+        address _signerAddress
+    ) external onlyRole(RESULT_MANAGER_ADMIN_ROLE) {
+        if (_signerAddress == address(0)) revert ZeroAddress();
+        emit SignerUpdated(msg.sender, signerAddress, _signerAddress);
+        signerAddress = _signerAddress;
+    }
+
+    /**
+     * @notice deletes the result of a collection
+     * @param name The name of the collection
+     */
+    function deleteResult(
+        bytes32 name
+    ) external onlyRole(RESULT_MANAGER_ADMIN_ROLE) {
+        delete _collectionResults[name];
+    }
+
+    /**  @notice Updates the result based on the provided Merkle proof and decoded result. Regardless of whether the result
+     * is updated, a result will be returned.
+     * @param merkleRoot The root of the Merkle tree
+     * @param proof The Merkle proof for the result
+     * @param result The decoded result
+     * @param signature The signature for the result
+     * @return result of the collection, its power and timestamp
+     */
+    function updateResult(
+        bytes32 merkleRoot,
+        bytes32[] memory proof,
+        Value memory result,
+        bytes memory signature
+    ) external onlyRole(FORWARDER_ROLE) returns (uint256, int8, uint256) {
+        if (
+            result.lastUpdatedTimestamp >
+            _collectionResults[result.name].lastUpdatedTimestamp
+        ) {
+            if (!_checkSignature(merkleRoot, signature))
+                revert InvalidSignature();
+
+            if (!_checkMerkle(result, merkleRoot, proof))
+                revert InvalidMerkleProof();
+
+            _collectionResults[result.name] = result;
+
+            emit ResultUpdated(result);
+        }
+
+        return _getResult(result.name);
+    }
+
+    /**
+     * @dev validates the result based on the provided data and returns the validity
+     * @param merkleRoot The root of the Merkle tree
+     * @param proof The Merkle proof for the result
+     * @param result The decoded result
+     * @param signature The signature for the result
+     * @return validity of the result
+     */
+    function validateResult(
+        bytes32 merkleRoot,
+        bytes32[] memory proof,
+        Value memory result,
+        bytes memory signature
+    )
+        external
+        view
+        onlyRole(FORWARDER_ROLE)
+        returns (bool, uint256, int8, uint256)
+    {
+        if (!_checkSignature(merkleRoot, signature)) return (false, 0, 0, 0);
+
+        if (!_checkMerkle(result, merkleRoot, proof)) return (false, 0, 0, 0);
+
+        return (true, result.value, result.power, result.lastUpdatedTimestamp);
+    }
+
+    /**
+     * @dev using the hash of collection name, clients can query the result of that collection
+     * @param _name bytes32 hash of the collection name
+     * @return result of the collection and its power
+     */
+    function getResult(
+        bytes32 _name
+    ) external view onlyRole(FORWARDER_ROLE) returns (uint256, int8, uint256) {
+        return _getResult(_name);
+    }
+
+    /**
+     * @dev internal function to check the signature of the result
+     * @param merkleRoot The root of the Merkle tree
+     * @param signature The signature for the result
+     * @return validity of the signature
+     */
+    function _checkSignature(
+        bytes32 merkleRoot,
+        bytes memory signature
+    ) internal view returns (bool) {
+        bytes32 messageHash = keccak256(abi.encodePacked(merkleRoot));
+
+        if (
+            ECDSA.recover(
+                ECDSA.toEthSignedMessageHash(messageHash),
+                signature
+            ) != signerAddress
+        ) return false;
+
+        return true;
+    }
+
+    /**
+     * @dev internal function to check the Merkle proof of the result
+     * @param result The decoded result
+     * @param merkleRoot The root of the Merkle tree
+     * @param proof The Merkle proof for the result
+     */
+    function _checkMerkle(
+        Value memory result,
+        bytes32 merkleRoot,
+        bytes32[] memory proof
+    ) internal pure returns (bool) {
+        bytes32 leaf = keccak256(bytes.concat(keccak256(abi.encode(result))));
+        if (!MerkleProof.verify(proof, merkleRoot, leaf)) return false;
+
+        return true;
+    }
+
+    /**
+     * @dev internal function where using the hash of collection name, clients can query the
+     * result of that collection
+     * @param _name bytes32 hash of the collection name
+     * @return result of the collection and its power
+     */
+    function _getResult(
+        bytes32 _name
+    ) internal view returns (uint256, int8, uint256) {
+        Value memory result = _collectionResults[_name];
+        if (result.value == 0) revert ZeroResult();
+        return (result.value, result.power, result.lastUpdatedTimestamp);
+    }
+}
